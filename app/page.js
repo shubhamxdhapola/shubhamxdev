ee8faddc87e1699caf00a27816ad56d21e1548f3
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-09-08 23:45:31 +0530

    Fixed theme issue

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    if (localStorage.theme === 'dark') {
       setIsDarkMode(true)
     } else {
       setIsDarkMode(false)
